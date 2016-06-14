aadcf3cf1c692790f123d7d4cb0fe613ae1026b6
Danny Valverde <djvalverde@gmail.com>
2016-06-14 13:18:19 -0600

    Problem1 Finished

--- problema1/problema1.sh
+++ problema1/problema1.sh
@@ -3,9 +3,11 @@
 DATA=/home/admin1/lx2_Danny/hojasExcel/hojasDatos
 OUT_DATA=$DATA/datos_csv
 GRAF_DATA=$DATA/datos_graf
+FULL_DATA=$DATA/full_datos
 
-#mkdir $DATA/archivos_csv
-#mkdir $GRAF_DATA
+mkdir $DATA/archivos_csv
+mkdir $GRAF_DATA
+mkdir $FULL_DATA
 
  m=0
 
@@ -27,3 +29,54 @@ cat $e | awk -F "\",\"" '{print $1 " " $2 " " $3 " "$4 " " $5}'| sed '1,$ s/"//g
 let m=m+1
 
 done 2> error2.log
+
+ # Este condicional elimina el archivo full.dat ya que si corre varias veces
+ # entonces se agregaran mas datos al archivo en lugar de crearlo con los
+ # datos generados. Osea se agregan por cada corrida un duplicado de los mismos
+ # datos.
+
+if [  a $FULL_DATA/full.dat ]
+then
+         rm $FULL_DATA/full.dat
+         echo "Archivo full.dat borrado"
+fi 2> errorIf.log
+
+
+for k in `find $GRAF_DATA  name "*.dat"`
+do
+sed '1d' $k >> $FULL_DATA/full.dat
+         echo "Procesando archivo $k"
+done 2> error3.log
+
+
+FMT_BEGIN='20110206 0000'
+FMT_END='20110206 0200'
+FMT_X_SHOW=%H:%M
+DATA_DONE=$FULL_DATA/full.dat
+
+## La linea set xrange que esta comentada deja en manos de gnuplot el
+
+## la seleccion del mejor rango en el eje x de forma que aparescan todos los 
+## datos. Si la descomentan entonces pueden manejar el despliegue de estos
+
+## a traves de las variables FMT_BEGIN y FMT_END. En este caso apareceran
+
+## todos los datos. Ver fig1.png donde aparecen todos los datos y en fig.png 
+## solo aparecen los datos del 6 de febrero como lo establecen las variables. 
+
+graficar()
+{
+gnuplot << EOF 2> error.log
+
+set xdata time
+set timefmt "%Y%m%d %H%M"
+# set xrange ["$FMT_BEGIN" : "$FMT_END"]
+set format x "$FMT_X_SHOW"
+set terminal png
+set output 'fig1.png'
+plot "$DATA_DONE" using 1:3 with lines title "sensor1", "$DATA_DONE" using 1:4 with linespoints title "sensor2"
+EOF
+
+}
+
+graficar
